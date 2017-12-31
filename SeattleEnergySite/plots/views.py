10e2c0d20443c378b9e7019323d2cc204229ec1e
Greg Stinson <stinsongr@gmail.com>
2017-12-30 19:03:40 -0800

    cleaned up formatting of plots

--- SeattleEnergySite/plots/views.py
+++ SeattleEnergySite/plots/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-import mpld3, pylab as plt, numpy as np, pdb
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt, mpld3, numpy as np, pdb
 
 from .models import Building, ASHRAE_target
 # Create your views here.
@@ -16,6 +18,8 @@ def index(request):
     targ_eui = ash_targ.values_list('target',flat=True)[0]
 
     f,ax=plt.subplots()
+    f.subplots_adjust(left=0.15,bottom=0.12)
+    matplotlib.rc('font',size=18)
     scatter = ax.scatter(b_objs.values_list(plot_type,flat=True),
                          b_objs.values_list('site_eui',flat=True))
     xlim=ax.get_xlim()
@@ -23,9 +27,17 @@ def index(request):
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
     med_eui = np.median(b_objs.values_list('site_eui',flat=True))
-    ax.plot(xlim,[targ_eui,targ_eui],'k--',label='ASHRAE target')
-    ax.plot(xlim,[med_eui,med_eui],'C0:',label='Median')
-    ax.legend()
+    ax.plot(xlim,[targ_eui,targ_eui],'k--',
+            label='ASHRAE target: %.1f'%(targ_eui))
+    ax.plot(xlim,[med_eui,med_eui],'C0:',label='Median: %.1f'%(med_eui))
+
+    ax.set_xlabel(plot_type,fontsize=18)
+    ax.set_ylabel('Energy Usage Intensity (EUI) [kBTU / sq ft / year]',fontsize=18)
+    ax.tick_params(axis='both',which='major',labelsize=18)
+    for tick in (ax.get_xticklabels()+ax.get_yticklabels()):
+        tick.set_fontname('Times')
+
+    ax.legend(loc=2)
     targets = ["http://blue.kingcounty.com/Assessor/eRealProperty/Detail.aspx?ParcelNbr="+f"{t:010}" for t in b_objs.values_list('Tax_PIN',flat=True)]
     tooltip = mpld3.plugins.PointHTMLTooltip(scatter,
                   labels=list(b_objs.values_list('Property_Name',flat=True)),
