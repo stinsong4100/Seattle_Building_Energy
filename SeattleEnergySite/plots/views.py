c5dcfdca6381591d63ebed69f3b001d01354289c
Greg Stinson <stinsongr@gmail.com>
2018-01-22 11:22:23 -0800

    made site_eui selection show a histogram rather than scatter

--- SeattleEnergySite/plots/views.py
+++ SeattleEnergySite/plots/views.py
@@ -3,7 +3,7 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt, mpld3, numpy as np, pdb
 
-from .models import Building, ASHRAE_target
+from .models import Building, ASHRAE_target, Lookup
 # Create your views here.
 
 def index(request):
@@ -14,35 +14,57 @@ def index(request):
         b_use = 'Office'
         plot_type = 'Year_remodeled'
     b_objs = Building.objects.filter(main_use=b_use)
+    b_euis=np.array(b_objs.values_list('site_eui',flat=True))
+    plot_type_values = b_objs.values_list(plot_type,flat=True)
+
     ash_targ = ASHRAE_target.objects.filter(main_use=b_use)
     targ_eui = ash_targ.values_list('target',flat=True)[0]
+    med_eui = np.median(b_euis)
 
     f,ax=plt.subplots()
     f.subplots_adjust(left=0.15,bottom=0.12)
     matplotlib.rc('font',size=18)
-    scatter = ax.scatter(b_objs.values_list(plot_type,flat=True),
-                         b_objs.values_list('site_eui',flat=True))
+    if plot_type=='site_eui':
+        # make histogram
+        hist_max=np.min([np.max(b_euis),5*med_eui])
+        ax.hist(b_euis,bins=20,range=[0,hist_max])
+    else:
+        scatter = ax.scatter(plot_type_values,b_euis)
+
     xlim=ax.get_xlim()
     ylim=ax.get_ylim()
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
-    med_eui = np.median(b_objs.values_list('site_eui',flat=True))
-    ax.plot(xlim,[targ_eui,targ_eui],'k--',
-            label='ASHRAE target: %.1f'%(targ_eui))
-    ax.plot(xlim,[med_eui,med_eui],'C0:',label='Median: %.1f'%(med_eui))
+
+    if plot_type=='site_eui':
+        # target and median EUI lines are vertical in this case
+        ax.plot([targ_eui,targ_eui],ylim,'k--',
+                label='ASHRAE target: %.1f'%(targ_eui))
+        ax.plot([med_eui,med_eui],ylim,'C1:',label='Median: %.1f'%(med_eui))
+        ax.legend(loc=1)
+    else:
+        # make scatter plot clickable and have labels
+        targets = ["http://blue.kingcounty.com/Assessor/eRealProperty/Detail.aspx?ParcelNbr="+f"{t:010}" for t in b_objs.values_list('Tax_PIN',flat=True)]
+        tooltip = mpld3.plugins.PointHTMLTooltip(scatter,
+                   labels=list(b_objs.values_list('Property_Name',flat=True)),
+                   targets=targets)
+        mpld3.plugins.connect(f,tooltip)
+        ax.plot(xlim,[targ_eui,targ_eui],'k--',
+                label='ASHRAE target: %.1f'%(targ_eui))
+        ax.plot(xlim,[med_eui,med_eui],'C0:',label='Median: %.1f'%(med_eui))
+        ax.set_ylabel('Energy Usage Intensity (EUI) [kBTU / sq ft / year]',fontsize=18)
+        ax.legend(loc=2)
+
+
+    if plot_type in Lookup.objects.values_list('case',flat=True).distinct():
+        # print text for x labels
+        case_objs=Lookup.objects.filter(case=plot_type)
+        plt.xticks(case_objs.values_list('code',flat=True),
+                   case_objs.values_list('value',flat=True))
 
     ax.set_xlabel(plot_type,fontsize=18)
-    ax.set_ylabel('Energy Usage Intensity (EUI) [kBTU / sq ft / year]',fontsize=18)
     ax.tick_params(axis='both',which='major',labelsize=18)
-    for tick in (ax.get_xticklabels()+ax.get_yticklabels()):
-        tick.set_fontname('Times')
 
-    ax.legend(loc=2)
-    targets = ["http://blue.kingcounty.com/Assessor/eRealProperty/Detail.aspx?ParcelNbr="+f"{t:010}" for t in b_objs.values_list('Tax_PIN',flat=True)]
-    tooltip = mpld3.plugins.PointHTMLTooltip(scatter,
-                  labels=list(b_objs.values_list('Property_Name',flat=True)),
-                  targets=targets)
-    mpld3.plugins.connect(f,tooltip)
     js_plot = mpld3.fig_to_html(f)
 
     uses = Building.objects.values_list('main_use',flat=True).distinct()
